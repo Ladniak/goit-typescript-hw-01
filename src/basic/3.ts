5f37e02b73152e37a3ef79d00413832b51c646a5
Netishyn <sergeyladnyak7@gmail.com>
2024-11-21 22:04:21 +0200

    fix 1 and 3 from basic and 2 and 3 from generics

--- src/basic/3.ts
+++ src/basic/3.ts
@@ -1,1 +1,13 @@
-let unionType: string | number;
+type unionType = string | number;
+
+let example: unionType;
+example = 123;
+example = "string";
+// example = true;   -   typescript mistake
+
+type status = "enable" | "disable";
+
+let currentStatus: status;
+currentStatus = "enable";
+currentStatus = "disable";
+// currentStatus = "on";   -   typescript mistake
